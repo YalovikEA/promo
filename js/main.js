f7ee3514ca8d8657b0fa7720f12c6eaeefb2aaa7
yalovikea <yalovikea@gmail.com>
2022-12-02 12:25:39 +0300

    feat: added header animation

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Links
+    const links = document.querySelectorAll('.socials__link');
 
+    links.forEach(el => {
+        el.addEventListener('click', (e) => {
+            if (el.classList.contains('disabled')) {
+                e.preventDefault();
+            }
+        })
+    })
+
+    // Form
+    const form = document.querySelector('.form');
+    const warning = document.querySelector('.socials__warning');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        links.forEach(el => {
+            el.classList.remove('disabled');
+        })
+        warning.remove();
+    })
+
+    // Animation
+    const body = document.querySelector('body');
+    const images = document.querySelector('.images');
+
+    images.addEventListener('mouseenter', () => {
+        body.classList.add('active')
+    })
+
+    images.addEventListener('mouseleave', () => {
+        body.classList.remove('active')
+    })
+});
